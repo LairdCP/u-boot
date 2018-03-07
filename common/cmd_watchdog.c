20340d4594f26876d1b4274ef1c5d15d8ccc79c7
Vishal Verma <vishal.verma@lairdtech.com>
2018-03-07 11:32:33 -0500

    laird: add watchdog driver support for SAMA5D3 series in uboot
    Created new watchdog driver for SAMA5D3 processor series. Also, included
    command "watchdog" in u-boot console to disable, enable and modify timeout
    time of the watchdog.
    
    bug 12502

---
+++ common/cmd_watchdog.c
@@ -0,0 +1,145 @@
+/*
+ * Watchdog driver command for Atmel SAMA5D3x processors series.
+ *
+ * (C) Copyright 2018
+ * Laird <ews-support@lairdtech.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+/*
+ *
+ * Watchdog commands to drive Atmel SAMA5D3x processor drivers in
+ * U-Boot
+ *
+ */
+ #include <common.h>
+ #include <command.h>
+ #include <linux/ctype.h> /* isdigit */
+ #ifndef CONFIG_HW_WATCHDOG
+ #define CONFIG_HW_WATCHDOG
+ #include <watchdog.h>
+ #endif
+
+/*
+ * To check if input argument is only a numeric value
+ *
+ */
+static bool is_number(const char* temp_arg)
+{
+    int pos = 0 ;
+
+    if(temp_arg[pos]== '-')
+    {
+        pos=1;
+    }
+
+    while(temp_arg[pos]!= 0)
+    {
+        if(!isdigit(temp_arg[pos]))
+        {
+            return false;
+        }
+        pos++;
+    }
+
+    return true;
+}
+
+
+/*
+ * Set watchdog environment variable
+ *
+ */
+static int do_env_set(const char* env_var, const char* env_value)
+{
+    setenv(env_var,env_value);
+    return 0;
+}
+
+
+static int do_watchdog(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+    int i = 1; /* Skip argv[0] (program name). */
+    int status = 1;
+    int input = -1;
+    if(argc < 2)
+    {
+        return CMD_RET_USAGE;
+    }
+
+    while (i < argc)
+    {
+        if (strcmp(argv[i], "-t") == 0)  /* Process optional arguments. */
+        {
+            if (i + 1 <= argc - 1)  /* There are enough arguments in argv. */
+            {
+                i++;
+                /*check if input arg is valid */
+                if(!(is_number(argv[i])))
+                {
+                    printf("\nERROR: Timeout value invalid\n");
+                    return CMD_RET_USAGE;
+                }
+                input = simple_strtol(argv[i],NULL,10);
+
+                /* Check if input is a number and in range */
+                if(!((input > 0) && (input <= 16)))
+                {
+                    printf("\nERROR: Out of range\n");
+                    return CMD_RET_USAGE;
+                }
+                status = do_env_set("timeout", argv[i]);
+            }
+
+            else
+            {
+                return CMD_RET_USAGE;
+            }
+        }
+
+        else
+        {
+            if(i == argc-1)
+            {
+                if(strcmp(argv[i], "on") == 0)
+                {
+                    status = do_env_set("watchdog", "on");
+                }
+
+                else if(strcmp(argv[i], "off") == 0)
+                {
+                    status = do_env_set("watchdog", "off");
+                }
+
+                else
+                {
+                    return CMD_RET_USAGE;
+                }
+            }
+
+            else
+            {
+                return CMD_RET_USAGE;
+            }
+
+        }
+        i++;
+    }
+
+    printf("Setting watchdog values... DONE\n");
+    printf("Writing watchdog values... \n");
+
+    if(!status)
+    {
+        saveenv();
+    }
+
+    return 0;
+}
+
+U_BOOT_CMD(watchdog, 4, 0,do_watchdog,
+	"Enable/Disable/Set <timeout> for Watchdog timer\n",
+	"[-t] <timeout> [on|off]\n"
+	"       - timeout in seconds (timeout is decimal and 0 < timeout <=16 !!!)"
+);
---
+++ drivers/watchdog/sama5d3_wdt.c
@@ -0,0 +1,134 @@
+/*
+ * Watchdog driver for Atmel SAMA5D3x processors series.
+ *
+ * Copyright (C) 2018
+ * Laird  <ews-support@lairdtech.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+/*
+ * The Watchdog Timer Mode Register can be only written to once. If the
+ * timeout need to be set from U-Boot, be sure that the bootstrap doesn't
+ * write to this register.
+ *
+ * Also, check if Watchdog Timer is configured as enabled in bootstrap
+ */
+
+ #include <common.h>
+ #include <watchdog.h>
+ #include <asm/arch/hardware.h>
+ #include <asm/io.h>
+ #include <asm/arch/at91_wdt.h>
+
+/*
+ * SAMA5D3x watchdog runs a 12bit counter @ 256Hz,
+ * use this to convert a watchdog
+ * value from/to milliseconds.
+ */
+ #define ms_to_ticks(t)	(((t << 8) / 1000) - 1)
+ #define ticks_to_ms(t)	(((t + 1) * 1000) >> 8)
+
+/*
+ * Default timeout to 16 seconds
+ */
+ #define DEFAULT_TIMEOUT 16
+
+/*
+ * Set the watchdog time interval in 1/256Hz (write-once)
+ * Counter is 12 bit.
+ */
+static int sama5d3_wdt_settimeout(unsigned int timeout)
+{
+    /* 16 seconds timer, resets enabled */
+    unsigned int reg;
+    at91_wdt_t *wd = (at91_wdt_t *) ATMEL_BASE_WDT;
+
+	/* Check if disabled */
+    if (readl(&wd->mr) & AT91_WDT_MR_WDDIS)
+    {
+        printf("sorry, Watchdog is disabled\n");
+
+        return -1;
+    }
+
+    /* Check timeout value if valid*/
+    if((timeout<=0) || (timeout>16))
+    {
+        printf("Watchdog: timeout value invalid\n");
+        printf("Watchdog: reseting timeout value to default\n");
+        timeout = DEFAULT_TIMEOUT;
+    }
+
+	/*
+	 * All counting occurs at SLOW_CLOCK / 128 = 256 Hz
+	 *
+	 * Since WDV is a 12-bit counter, the maximum period is
+	 * 4096 / 256 = 16 seconds.
+	 */
+    reg =AT91_WDT_MR_WDRSTEN /* reset enabled*/
+        | AT91_WDT_MR_WDDBGHLT /* disabled in debug mode */
+        | AT91_WDT_MR_WDD(0xfff) /* restart at any time */
+        | AT91_WDT_MR_WDV(ms_to_ticks(timeout * 1000));	/* timeout value */
+
+    printf("Watchdog: timeout %d secs",timeout);
+    writel(reg, &wd->mr);
+
+    return 0;
+}
+
+/*
+ * Disable Watchdog
+ *
+ */
+static void sama5d3_wdt_disable(void)
+{
+    at91_wdt_t *wd = (at91_wdt_t *) ATMEL_BASE_WDT;
+    printf("Watchdog: Disabled\n");
+    writel(AT91_WDT_MR_WDDIS,&wd->mr);
+}
+
+/*
+ * Reset watchdog
+ *
+ */
+void hw_watchdog_reset(void)
+{
+    at91_wdt_t *wd = (at91_wdt_t *) ATMEL_BASE_WDT;
+    printf("Watchdog: Reset\n");
+    writel(AT91_WDT_CR_WDRSTT | AT91_WDT_CR_KEY, &wd->cr);
+}
+
+/*
+ * Initialize watchdog
+ *
+ */
+void hw_watchdog_init(void)
+{
+    const char *watchdog= getenv("watchdog");
+    unsigned int timeout= (unsigned int) getenv_ulong("timeout",10,DEFAULT_TIMEOUT);
+
+    if(watchdog)
+    {
+        if((strcmp("off",watchdog) == 0))
+        {
+            sama5d3_wdt_disable();
+        }
+
+        else if(strcmp("on",watchdog) == 0)
+        {
+            sama5d3_wdt_settimeout(timeout);
+        }
+
+        else
+        {
+            sama5d3_wdt_disable(); // to disable by default
+        }
+
+    }
+
+    else
+    {
+        sama5d3_wdt_disable();
+    }
+}
